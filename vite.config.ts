ca770fe9eb9801ee6618594219c9c0e7e67c15c2
xkfe <zkai98516@gmail.com>
2023-08-22 23:23:49 +0800

    chore: 引入unplugin-auto-import并配置自动导入vue、uniapp api

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+// 引入自动导入插件
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    // 配置自动导入 vue相关函数, uni-app相关函数。ref, reactive，onLoad等
+    AutoImport({
+      imports: ['vue','uni-app'],
+      dts: './typings/auto-imports.d.ts',
+    }),
+  ],
 });
